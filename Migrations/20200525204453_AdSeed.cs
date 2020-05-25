9295d327358555b08418a2cdc9069ed3ef998700
AJacobs94 <56659280+AJacobs94@users.noreply.github.com>
2020-05-25 15:46:43 -0500

    AdSeed

---
+++ Migrations/20200525204453_AdSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LiftoffRMAV.Migrations
+{
+    public partial class AdSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
